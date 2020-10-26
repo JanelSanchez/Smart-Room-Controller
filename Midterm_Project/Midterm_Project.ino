b9ef01287bb3616182c774ff352ffdd9affbe66b
JanelSanchez <JanelASanchez@gmail.com>
2020-10-26 10:17:52 -0600

    Midterm Project Begins...

---
+++ Midterm_Project/Midterm_Project.ino
@@ -0,0 +1,16 @@
+/*
+ * Project: Midterm Project
+ * Description: Smart Room Controller
+ * Author: Janel Sanchez
+ * Date: 26-Oct-2020
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
