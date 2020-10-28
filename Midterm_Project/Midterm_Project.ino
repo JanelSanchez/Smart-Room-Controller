70eb1f548ea90faa8f5f39939c060a8e3ea8a898
JanelSanchez <JanelASanchez@gmail.com>
2020-10-28 15:09:24 -0600

    Step1_Complete-Ultra

--- Midterm_Project/Midterm_Project.ino
+++ Midterm_Project/Midterm_Project.ino
@@ -2,83 +2,42 @@
  * Project: Midterm Project
  * Description: Smart Room Controller
  * Author: Janel Sanchez
- * Date: 27-Oct-2020
+ * Date: 28-Oct-2020
 */
 
 #include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_BME280.h>
 #include <Ethernet.h>
-#include <mac.h>
 #include <Encoder.h>
-#include <wemo.h>
-#include <Adafruit_NeoPixel.h>
-#include <colors.h>
-
-Adafruit_BME280 bme;
-
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     9 // Reset pin # (or -1 if sharing Arduino reset pin)
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-const char degreeSymbol = 247;
-
-float tempC;
-float tempF = 0;
-
-const int pinA = 3;
-const int pinB = 2;
-const int encButton = 23;
-
-const int pinPixelStrip = 20;
-const int pixelCount = 60;
-
-const int buttonPinBlue = 14;
-bool buttonStateBlue = 0;
-const int ledPinBlue = 6;
-
-const int buttonPinYellow = 15;
-bool buttonStateYellow = 0;
-const int ledPinYellow = 7;
-
-const int buttonPinRed = 16;
-bool buttonStateRed = 0;
-const int ledPinRed = 8;
-
-Encoder myEnc(3,2);
-Adafruit_NeoPixel pixel (pixelCount,pinPixelStrip, NEO_GRB + NEO_KHZ800);
-
-int encPos;
-int pos;
-int pixelPos;
+#include <mac.h>
 
 EthernetClient client;
-bool status;
-bool lastButtonBlue;
-bool lastButtonYellow;
-bool lastButtonRed;
-bool wemoState;
 
-Wemo wemo;
+// Declare Bools
+bool status;
+bool lastUltra;
+bool roomOccupied;
+
+
+// Declare Constants
+const int echoPin=1; // attach digital pin Echo of HC-SR04
+const int trigPin=0; //attach digital pin Trig of HC-SR04
+const int nssPin=10; //attach pin nSS of Ethernet
+
+
+// Declare Variables 
+int ultraState;   // variable for the distance measurement
+
 
 void setup() {
+  pinMode(nssPin, OUTPUT); //Sets the nssPin as an OUTPUT
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   
-  pinMode(10, OUTPUT);      //ethernet+wemo setup begin
-  digitalWrite(10, HIGH);
-
-  pinMode(4, OUTPUT);
-  digitalWrite(4, HIGH);
-
-  //Open Serial Communication and wait for port to open:
   Serial.begin(9600);
-  while (!Serial);
+  delay(100);         // wait for Serial Monitor to Open 
+  Serial.println("Starting my AMAZING Smart Room Controller Project Program");
 
-  Serial.println("Starting Program");
-
-  //Start ethernet connection
+//Start ethernet connection
   status = Ethernet.begin(mac);
   if (!status) {
     Serial.println("failed to configure Ethernet using DHCP");
@@ -92,83 +51,47 @@ void setup() {
     Serial.print(Ethernet.localIP()[thisbyte], DEC);
     if (thisbyte < 3) Serial.print(".");
     }
-  Serial.println();         //ethernet+wemo setup end
-
-  pinMode (buttonPinBlue,INPUT);    //button+LED setup begin
-  pinMode (ledPinBlue, OUTPUT);
-  pinMode (buttonPinYellow,INPUT);
-  pinMode (ledPinYellow, OUTPUT);
-  pinMode (buttonPinRed,INPUT);
-  pinMode (ledPinRed, OUTPUT);      //button+LED setup end
-
-  pixel.begin();                    //Pixel setup begin
-  pixel.show();
-  pixel.setBrightness(88);          //Pixel setup end
-
-  status = bme.begin(0x76);           //BME setup begin
-  if(!status) {
-    Serial.println("Initialization Failed");
-  }                                   //BME setup end
+  Serial.println();
 }
 
-void loop(){
-  
+void loop() {
+  ultraState=ultra();
+  if(ultraState!=lastUltra) {
+    if(ultraState == true) {
+      roomOccupied = !roomOccupied;
+    }
+    lastUltra=ultraState;
+  }
+  if(roomOccupied == true) {
+    Serial.println("SOMEONE IS IN THE ROOM");
+  }
+  else {
+    Serial.println("NO ONE IS IN THE ROOM");
+  }  
 }
 
-
-void threebutton() {
-  // put your main code here, to run repeatedly:
-  buttonStateBlue=digitalRead(buttonPinBlue);     //Button+Wemo Loop Start
-  if(buttonStateBlue!=lastButtonBlue) {
-    if(buttonStateBlue == HIGH) {
-      wemoState = !wemoState;
-    }
-    lastButtonBlue=buttonStateBlue;
-  }
-  if(wemoState == HIGH) {
-    wemo.switchON(0);
-  }
-  if(wemoState == LOW) {
-    wemo.switchOFF(0);  
-  }
-
-  buttonStateYellow=digitalRead(buttonPinYellow);
-  if(buttonStateYellow!=lastButtonYellow) {
-    if(buttonStateYellow == HIGH) {
-      wemoState = !wemoState;
-    }
-    lastButtonYellow=buttonStateYellow;
-  }
-  if(wemoState == HIGH) {
-    wemo.switchON(1);
-  }
-  if(wemoState == LOW) {
-    wemo.switchOFF(1);  
-  }
+bool ultra() {
+  int duration;
+  int distance;
   
-  buttonStateRed=digitalRead(buttonPinRed);
-  if(buttonStateRed!=lastButtonRed) {
-    if(buttonStateRed == HIGH) {
-      wemoState = !wemoState;
-    }
-    lastButtonRed=buttonStateRed;
-  }
-  if(wemoState == HIGH) {
-    wemo.switchON(2);
-  }
-  if(wemoState == LOW) {
-    wemo.switchOFF(2);  
-  }                                                 //Button+Wemo Loop End
-
-}
-
-void temp() {
-  tempC = bme.readTemperature();
-  pressPA = ((bme.readPressure()/100.0)*0.03);
-  humidRH = bme.readHumidity();
+  delay(1000); // Execute once per second 
   
-  tempF = (tempC*1.8)+32;
+  // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
 
-  Serial.printf("Temp %0.1f°F \n", tempF);
-  delay(50000);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds, this is the pulse that will be detected
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  Serial.printf("Time %i, Distance: %i, Duration %i \n",millis(),distance, duration);
+  if(distance<100) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
