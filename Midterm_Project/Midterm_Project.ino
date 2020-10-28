20f62976a7ca96a6ce16db17eb34c0c7559a9d72
JanelSanchez <JanelASanchez@gmail.com>
2020-10-28 11:06:50 -0600

    Step1_Completed - Used Ultra to create an auto button read for when someone enters a room

--- Midterm_Project/Midterm_Project.ino
+++ Midterm_Project/Midterm_Project.ino
@@ -20,7 +20,7 @@
 Adafruit_BME280 bme;
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     9 // Reset pin # (or -1 if sharing Arduino reset pin)
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
